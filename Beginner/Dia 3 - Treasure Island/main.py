6605f498e0dbf592ceaf0a99842ab82c5aed2fcd
Adrian Garcia <agp8119@gmail.com>
2021-08-19 16:53:57 +0200

    Modificado dia 3 Lower

--- Beginner/Dia 3 - Treasure Island/main.py
+++ Beginner/Dia 3 - Treasure Island/main.py
@@ -23,14 +23,14 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-cross_road = input("You're at a cross road. Where do you want to go? 'left' or 'right' ")
+cross_road = input("You're at a cross road. Where do you want to go? 'left' or 'right' ").lower()
 
 
 
 if (cross_road == 'left'):
-    lake = input("You come to a lake. There is a island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ")
+    lake = input("You come to a lake. There is a island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ").lower()
     if (lake == 'wait'):
-        island = input("You arrived at the island unharmed. There is a house with 3 doors. One red, one yellow an one blue. Which color do you choose? ")
+        island = input("You arrived at the island unharmed. There is a house with 3 doors. One red, one yellow an one blue. Which color do you choose? ").lower()
         if (island == 'red'):
             print("Burned by fire. Game Over.")
         elif (island == 'blue'):
